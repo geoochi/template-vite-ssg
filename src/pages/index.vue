1fe9da3dab3f6a14512184dd25b80a08fbd041dd
Geoo Chi <geoochi@geoochi.com>
2025-04-05 15:46:50 +0800

    add count

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import { useHead } from '@unhead/vue'
+import { ref } from 'vue'
+import { Button } from '@/components/ui/button'
 
 useHead({ title: 'home page' })
+const count = ref(0)
 </script>
 
 <template>
   <p>Home Page</p>
   <RouterLink to="/about" class="underline">link to about</RouterLink>
+  <Button @click="count++">count: {{ count }}</Button>
 </template>
